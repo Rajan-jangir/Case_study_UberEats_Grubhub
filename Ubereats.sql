6538a917cbfafb19d4a6baefaa7fd24d45a658fa
Rajan Jangir <62979795+Rajan-jangir@users.noreply.github.com>
2024-10-26 20:46:27 +0530

    Update Ubereats.sql

--- Ubereats.sql
+++ Ubereats.sql
@@ -3,10 +3,14 @@ SELECT *
 FROM `arboreal-vision-339901.take_home_v2.virtual_kitchen_ubereats_hours`
 LIMIT 5;
 
--- Reviewed schema and retrieving reg_hours
-SELECT
-      JSON_EXTRACT(value, "$.regularHours.endTime") AS end_time,  
-      JSON_EXTRACT(value, "$.regularHours.startTime") AS start_time
-FROM 
-  `arboreal-vision-339901.take_home_v2.virtual_kitchen_ubereats_hours`,
-  UNNEST(JSON_QUERY_ARRAY(response, '$.data.menus.sections')) as value
+-- Checking relavant fields
+ SELECT
+   slug AS ue_slug,
+   JSON_VALUE(response, '$.data.menus."26bd579e-5664-4f0a-8465-2f5eb5fbe705".sections[0].regularHours[0].startTime') AS Ubereats_starttime,
+   JSON_VALUE(response, '$.data.menus."26bd579e-5664-4f0a-8465-2f5eb5fbe705".sections[0].regularHours[0].endTime') AS Ubereats_endtime,
+   STRUCT(
+     b_name AS b_name,
+     vb_name AS vb_name
+   ) AS restaurant_info
+ FROM
+   `arboreal-vision-339901.take_home_v2.virtual_kitchen_ubereats_hours`;
